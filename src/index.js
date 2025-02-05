58577793d67d411fa3e727302762d8f7aa27c47f
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-02-05 15:13:21 -0300

    /

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { initializeApp } from '/firebase/app';
-import { getAuth, onAuthStateChanged } from '/firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 
 const firebaseApp = initializeApp({
